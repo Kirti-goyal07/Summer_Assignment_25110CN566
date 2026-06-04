15f7400a1e3300e7986db052aa7614e18f2662b2
Kirti-goyal07 <goyalkirti797@gmail.com>
2026-06-04 21:26:04 +0530

    Day-4 uploaded

---
+++ Day-4/Q16.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int start, end, num, temp, rem, digits;
+    double sum;
+
+    printf("Enter start and end of range: ");
+    scanf("%d %d", &start, &end);
+
+    printf("Armstrong numbers are:\n");
+
+    for (num = start; num <= end; num++) {
+        temp = num;
+    digits = 0;
+    sum = 0;
+
+        while (temp != 0) {
+            digits++;
+            temp /= 10;
+        }
+
+        temp = num;
+
+        while (temp != 0) {
+            rem = temp % 10;
+            sum += pow(rem, digits);
+            temp /= 10;
+        }
+
+        if (sum == num)
+            printf("%d ", num);
+    }
+
+    return 0;
+}
