724d551459b8938026563cd32e1cc6dc3a40565e
Kirti-goyal07 <goyalkirti797@gmail.com>
2026-06-09 18:23:25 +0530

    Day-9 uploaded

---
+++ Day-9/Q35.java
@@ -0,0 +1,12 @@
+public class Q35{
+    public static void main(String[]args){
+        char letter = 'A';
+        for(int i =1; i<=5;i++){
+            for(int j =1;j<=i;j++){
+                System.out.print(letter + " ");
+            }
+            letter++;
+            System.out.println();
+        }
+    }
+}
